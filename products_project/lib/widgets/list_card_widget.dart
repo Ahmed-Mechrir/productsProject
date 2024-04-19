611d7df1bacdc79120947605adefd3545bb8de95
Ahmed Mechrir <ahmedmechrir@yahoo.com>
2024-04-19 10:34:56 +0100

    Create project structure

---
+++ products_project/lib/widgets/list_card_widget.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ListCardWidget extends StatelessWidget {
+  const ListCardWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
